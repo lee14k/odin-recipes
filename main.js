c1a9a5fd1685fd0e687b3ef35b6c371fda2c69a0
lee14k <kmkovach@mtu.edu>
2022-10-24 23:17:00 -0400

    implemented javascript, new nav bar
    working on recipe card functionality, new navigation bar

---
+++ main.js
@@ -0,0 +1,23 @@
+let panel = document.querySelector('.arrow1')
+let slidey = document.querySelector('.exist')
+let panel2 = document.querySelector('.arrow2')
+let c=0
+panel.addEventListener('click', slider)
+function slider () {
+if (c%2 === 0) {
+    document.querySelector('.exist').className ='exist panel'
+    c++
+}else {
+    document.querySelector('.exist').className='exist'
+c++
+}
+}
+
+
+
+//panel2.addEventListener ('click', slideBack)
+
+//function slideBack () {
+  //  slidey.classList.add('nopanel')}
+
+//for (i=0;i<panel.length)
